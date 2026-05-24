a34d2bf5fb4576ce072954ff4db43ce319d9a7a6
Vivek Gohil - Desktop <ghl_vivek@hotmail.com>
2026-05-24 23:15:17 +0530

    Main method changes

--- src/com/borntocode/main/BankingApplicationMain.java
+++ src/com/borntocode/main/BankingApplicationMain.java
@@ -1,7 +1,14 @@
 package com.borntocode.main;
 
+import com.borntocode.main.domain.Account;
+
 public class BankingApplicationMain {
 	public static void main(String[] args) {
-		System.out.println("Banking Application Project");
+		System.out.println("Banking Application");
+		Account account = new Account(104, "Test", 10000);
+		if (account.withdraw(1000))
+			System.out.println("Withdraw successfull!!");
+		else
+			System.out.println("Withdraw failed");
 	}
 }
