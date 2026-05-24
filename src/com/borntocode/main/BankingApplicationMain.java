e953e8ba9a8f2711fcf85e666889dc334915bed3
Vivek Gohil - Laptop <borntocode2018@gmail.com>
2026-05-24 23:22:30 +0530

    Current Account

--- src/com/borntocode/main/BankingApplicationMain.java
+++ src/com/borntocode/main/BankingApplicationMain.java
@@ -4,7 +4,7 @@ import com.borntocode.main.domain.Account;
 
 public class BankingApplicationMain {
 	public static void main(String[] args) {
-		System.out.println("Banking Application");
+		System.out.println("Current Banking Application");
 		Account account = new Account(104, "Test", 10000);
 		if (account.withdraw(1000))
 			System.out.println("Withdraw successfull!!");
